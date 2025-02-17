0eda2f2d60a923306cdfc2aa4048bdd769cbc5f0
Bharadwaj Aaditya Ghali <91147921+Aadigha-git@users.noreply.github.com>
2025-02-17 15:17:06 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  trailingSlash: true, 
+  basePath: "/BharadwajGhali", 
+  assetPrefix: "/BharadwajGhali/",
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
 };
-trailingSlash: true, // Ensures correct routing on GitHub Pages
-  images: {
-    unoptimized: true, // Disables Image Optimization (GitHub Pages does not support Next.js image optimization)
-  },
-  basePath: "/BharadwajGhali", // Set this to match your repository name
-  assetPrefix: "/BharadwajGhali/",
-};
-
 
 module.exports = nextConfig;
