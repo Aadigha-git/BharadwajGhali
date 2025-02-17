ca88d977f8654a06214cf0b81a222fe0030a02b0
Bharadwaj Aaditya Ghali <91147921+Aadigha-git@users.noreply.github.com>
2025-02-17 11:16:38 -0800

    Update experience.ts

--- data/experience.ts
+++ data/experience.ts
@@ -1,30 +1,27 @@
 export interface Experience {
   company: string;
   role: string;
+  location: string;
   duration: string;
   description: string;
 }
 
 export const experiences: Experience[] = [
   {
-    company: "Smartdocs Inc",
-    role: "Software Development Engineer",
-    duration: "Aug 2024 - Nov 2024",
+    company: "Indegene",
+    role: "Gen AI Intern",
+    location: "Bengaluru, KA, IN",
+    duration: "Jul 2024",
     description:
-      "Developed and deployed 3 production applications and 2 proof-of-concept projects. Built a real-time chatbot with optimized LLM context tokens and created a Thick client application using ElectronJS, reducing yearly Jenkins server costs.",
+      "Contributed to Indegene's AI initiatives by conducting in-depth research on cutting-edge AI technologies, with a focus on fine-tuning techniques. Played a pivotal role in initiating the exploration of the DSPy framework for potential applications within the organization. My research was instrumental in informing senior stakeholders about the feasibility of integrating these advancements into the product roadmap.",
   },
   {
-    company: "Anti AI",
-    role: "Software Development Engineer",
-    duration: "Mar 2024 - July 2024",
+    company: "thinkMolecular",
+    role: "Intern",
+    location: "Bengaluru, KA, IN",
+    duration: "Jan 2024 - Feb 2024",
     description:
-      "Led development of company's first PoC software release. Developed cross-platform releases using React, Next.js, ElectronJS, and PostgreSQL.",
+      "Designed and implemented Python scripts leveraging RDKit and DecimerAI to precisely extract chemical structures from patent data. Collaborated closely with data science engineers to optimize structure extraction for downstream analysis. Enhanced script efficiency and adaptability by improving modularity and reusability for seamless integration into data pipelines.",
   },
-  {
-    company: "NIT Trichy",
-    role: "Summer Research Intern",
-    duration: "Jun 2023 - Aug 2023",
-    description:
-      "Worked on Microsoft's CyberBattleSim project, implementing DQL/PPO Learning Algorithms. Co-authored a book chapter on Cyberdeception for Detection of Lateral Movement in Enterprise Networks.",
-  },
+
 ];
